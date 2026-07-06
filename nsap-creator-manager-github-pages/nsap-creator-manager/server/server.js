faac16961d2e818b43269a052c7876057ea1335f
Lunyxzz <stoonspy@gmail.com>
2026-07-06 14:35:07 +0300

    Delete nsap-creator-manager-github-pages/nsap-creator-manager directory

--- nsap-creator-manager-github-pages/nsap-creator-manager/server/server.js
+++
@@ -1,8 +0,0 @@
-const { port } = require("./config");
-const { createApp } = require("./app");
-
-const app = createApp();
-
-app.listen(port, () => {
-  console.log(`NSAP Creator Manager listening on port ${port}`);
-});
